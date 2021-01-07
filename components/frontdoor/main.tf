a07b5a3789429ac8aa226e2167c8bc69418f69bd
mokainos <72137340+mokainos@users.noreply.github.com>
2021-01-07 13:33:29 +0000

    updated tfvars

--- components/frontdoor/main.tf
+++ components/frontdoor/main.tf
@@ -7,7 +7,9 @@ module "logworkspace" {
 
 
 module "landing_zone" {
-  source = "git::https://github.com/hmcts/terraform-module-frontdoor.git?ref=master"
+  #source = "git::https://github.com/hmcts/terraform-module-frontdoor.git?ref=master"
+  source = "git::https://github.com/hmcts/terraform-module-frontdoor.git?features/dtspo_242_data_provider"
+
 
   providers = {
     azurerm      = azurerm
