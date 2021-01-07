369984f1366b0050c1e072b69d7630d65d8e5a56
mokainos <72137340+mokainos@users.noreply.github.com>
2021-01-07 09:44:38 +0000

    added appgateway entries

--- components/frontdoor/init.tf
+++ components/frontdoor/init.tf
@@ -15,9 +15,3 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-provider "azurerm" {
-  features {}
-  alias           = "loganalytics"
-  subscription_id = local.log_analytics_workspace[[for x in keys(local.log_analytics_env_mapping) : x if contains(local.log_analytics_env_mapping[x], var.env)][0]].subscription_id
-}
