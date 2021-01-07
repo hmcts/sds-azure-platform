420b59783f24552cb2dbd281ab5acc89f5e0b311
mokainos <72137340+mokainos@users.noreply.github.com>
2021-01-07 13:23:57 +0000

    updated tfvars

--- components/appgateway/init.tf
+++ components/appgateway/init.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 0.14.4"
 
   backend "azurerm" {
     subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
@@ -8,7 +8,7 @@ terraform {
     azurerm = {
       source = "hashicorp/azurerm"
       # https://github.com/terraform-providers/terraform-provider-azurerm/issues/8208
-      version = "2.23.0"
+      version = "2.41.0"
     }
   }
 }
