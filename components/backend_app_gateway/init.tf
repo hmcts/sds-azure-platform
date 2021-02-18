71c2014d4a927840763885808f791edbb9ce400e
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-18 15:37:55 +0000

    DTSPO-658 Remove features

--- components/backend_app_gateway/init.tf
+++ components/backend_app_gateway/init.tf
@@ -1,12 +1,12 @@
 terraform {
   required_version = ">= 0.14.4"
 
-provider "azurerm" {
-  version = "2.41.0"
-  features {}
-  /* alias           = "loganalytics"
+  provider "azurerm" {
+    version = "2.41.0"
+    features {}
+    /* alias           = "loganalytics"
   subscription_id = local.log_analytics_workspace[[for x in keys(local.log_analytics_env_mapping) : x if contains(local.log_analytics_env_mapping[x], var.env)][0]].subscription_id */
-}
+  }
 
   backend "azurerm" {
     subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
