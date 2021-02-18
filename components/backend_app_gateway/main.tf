81d0f6be2c91e8cca817af7458590fa6bc14ef10
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-18 11:28:00 +0000

    DTSPO-658 Ammend main.tf

--- components/backend_app_gateway/main.tf
+++ components/backend_app_gateway/main.tf
@@ -7,14 +7,14 @@ module "logworkspace" {
 module "backend_app_gateway" {
   source = "git::https://github.com/hmcts/terraform-module-application-backend.git?ref=master"
 
-  env                       = var.env
-  subscription              = var.subscription
-  vault_name                = var.vault_name
-  location                  = var.location
-  backend_pool_ip_addresses = var.backend_pool_ip_addresses
-  private_ip_address        = var.private_ip_address
-  yaml_path                 = var.yaml_path
-  oms_env                   = var.oms_env
+  env                = var.env
+  subscription       = var.subscription
+  vault_name         = var.vault_name
+  location           = var.location
+  ip_addresses       = var.backend_pool_ip_addresses
+  private_ip_address = var.private_ip_address
+  yaml_path          = var.yaml_path
+  oms_env            = var.oms_env
   // log_analytics_workspace_id = module.logworkspace.workspace_id
 
 }
