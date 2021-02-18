7a4b8d1f52aaa3e4c41e8c66ec916491657903e1
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-18 15:57:45 +0000

    DTSPO-658

--- components/backend_app_gateway/init.tf
+++ components/backend_app_gateway/init.tf
@@ -1,18 +1,19 @@
 terraform {
-  required_version = ">= 0.14.3"
+  required_version = ">= 0.14.4"
 
   backend "azurerm" {
     subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
   }
+
+  provider "azurerm" {
+  features {}
+
+}
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "2.41.0"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
-
 }
