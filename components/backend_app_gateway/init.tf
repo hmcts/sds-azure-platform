d09e5616e8911a758a64be9af7485d82313c7216
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-18 18:44:06 +0000

    DTSPO-658 Add log analytics

--- components/backend_app_gateway/init.tf
+++ components/backend_app_gateway/init.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.14.4"
+  required_version = ">= 0.14.3"
 
   backend "azurerm" {
     subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
@@ -7,13 +7,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.41.0"
+      version = "2.41.0"
     }
   }
 }
 
 provider "azurerm" {
-  subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
   features {}
+}
 
+provider "azurerm" {
+  features {}
+  alias           = "loganalytics"
+  subscription_id = local.log_analytics_workspace[[for x in keys(local.log_analytics_env_mapping) : x if contains(local.log_analytics_env_mapping[x], var.env)][0]].subscription_id
 }
