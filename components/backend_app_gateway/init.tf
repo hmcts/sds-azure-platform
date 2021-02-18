1fc8bd7be7107e5805de92b33a256edfdd1c83df
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-18 16:58:10 +0000

    DTSPO-658 Add features

--- components/backend_app_gateway/init.tf
+++ components/backend_app_gateway/init.tf
@@ -1,24 +1,18 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
-
-}
-
 terraform {
   required_version = ">= 0.14.4"
 
-  /* backend "azurerm" {
+  backend "azurerm" {
     subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
-  } */
+  }
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "2.41.0"
     }
   }
 }
-/* 
+
 provider "azurerm" {
   features {}
-  alias           = "loganalytics"
-  subscription_id = local.log_analytics_workspace[[for x in keys(local.log_analytics_env_mapping) : x if contains(local.log_analytics_env_mapping[x], var.env)][0]].subscription_id
-} */
+
+}
