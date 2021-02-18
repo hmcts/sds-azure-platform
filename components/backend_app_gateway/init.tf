00c36419a317102033b13b6a2a8beb9be2f16fba
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-18 15:23:57 +0000

    DTSPO-658 Add features

--- components/backend_app_gateway/init.tf
+++ components/backend_app_gateway/init.tf
@@ -5,7 +5,6 @@ terraform {
     subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
   }
   required_providers {
-    features {}
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "2.41.0"
