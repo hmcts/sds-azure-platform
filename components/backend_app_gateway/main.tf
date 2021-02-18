390de82226e119ab1176e64232c15d2f71871280
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-18 14:29:21 +0000

    add lb config

--- components/backend_app_gateway/main.tf
+++ components/backend_app_gateway/main.tf
@@ -7,6 +7,8 @@ module "logworkspace" {
 module "backend_app_gateway" {
   source = "git::https://github.com/hmcts/terraform-module-application-backend.git?ref=master"
 
+  yaml_path = "${path.cwd}${local.env}/backend_lb_config.yaml"
+
   env                       = var.env
   subscription              = var.subscription
   vault_name                = var.vault_name
@@ -14,9 +16,6 @@ module "backend_app_gateway" {
   backend_pool_ip_addresses = var.backend_pool_ip_addresses
   private_ip_address        = var.private_ip_address
   oms_env                   = var.oms_env
-  yaml_path                 = var.yaml_path
   // log_analytics_workspace_id = module.logworkspace.workspace_id
 
 }
-
-yaml_path = "${path.cwd}${local.env}/backend_lb_config.yaml"
