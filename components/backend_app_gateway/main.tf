326f8daa7957cc386384473c8e0dc2aaa337cda7
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-18 12:29:31 +0000

    DTSPO-658 Add formatting

--- components/backend_app_gateway/main.tf
+++ components/backend_app_gateway/main.tf
@@ -7,10 +7,10 @@ module "logworkspace" {
 module "backend_app_gateway" {
   source = "git::https://github.com/hmcts/terraform-module-application-backend.git?ref=master"
 
-  env          = var.env
-  subscription = var.subscription
-  vault_name   = var.vault_name
-  location     = var.location
+  env                = var.env
+  subscription       = var.subscription
+  vault_name         = var.vault_name
+  location           = var.location
   ip_addresses       = var.backend_pool_ip_addresses
   private_ip_address = var.private_ip_address
   yaml_path          = var.yaml_path
