e44631c5e55ae33d0659d17bb2dbcf24919c0b98
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-19 12:18:42 +0000

    DTSPO-658 Add log analytics config

--- components/backend_app_gateway/main.tf
+++ components/backend_app_gateway/main.tf
@@ -9,14 +9,14 @@ module "backend_app_gateway" {
 
   yaml_path = "${path.cwd}/backend_lb_config.yaml"
 
-  env                       = var.env
-  subscription              = var.subscription
-  vault_name                = var.vault_name
-  location                  = var.location
-  backend_pool_ip_addresses = var.ss_apps_cluster_ips
-  private_ip_address        = var.private_ip_address
-  oms_env                   = var.oms_env
-  log_analytics             = module.logworkspace.workspace_id
-  appgwbackends             = var.appgwbackends
+  env                        = var.env
+  subscription               = var.subscription
+  vault_name                 = var.vault_name
+  location                   = var.location
+  backend_pool_ip_addresses  = var.ss_apps_cluster_ips
+  private_ip_address         = var.private_ip_address
+  oms_env                    = var.oms_env
+  log_analytics_workspace_id = module.logworkspace.workspace_id
+  // appgwbackends             = var.appgwbackends
 
 }
