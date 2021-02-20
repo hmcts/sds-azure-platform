e5c6061ddc4da21facb71e3dc1147013947aad3a
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-02-20 00:10:31 +0000

    DTSPO-658 Ammend init.tf

--- components/backend_app_gateway/init.tf
+++ components/backend_app_gateway/init.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.14.3"
+  required_version = "0.14.4"
 
   backend "azurerm" {
     subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.41.0"
+      version = "2.41.0"
     }
   }
 }
@@ -17,8 +17,8 @@ provider "azurerm" {
   features {}
 }
 
-/* provider "azurerm" {
-  features {}
-  alias           = "loganalytics"
-  subscription_id = local.log_analytics_workspace[[for x in keys(local.log_analytics_env_mapping) : x if contains(local.log_analytics_env_mapping[x], var.env)][0]].subscription_id
-} */
+// provider "azurerm" {
+//  features {}
+//  alias           = "loganalytics"
+//  subscription_id = local.log_analytics_workspace[[for x in keys(local.log_analytics_env_mapping) : x if contains(local.log_analytics_env_mapping[x], var.env)][0]].subscription_id
+//}
