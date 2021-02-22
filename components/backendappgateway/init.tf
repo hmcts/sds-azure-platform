4242fa6c551287de671ef5a1d059d570bba1f1ee
Matt Slater <matt.slater@hmcts.net>
2021-02-22 19:57:41 +0000

    DTSPO-658 - testing

--- components/backendappgateway/init.tf
+++ components/backendappgateway/init.tf
@@ -14,6 +14,7 @@ terraform {
 provider "azurerm" {
   alias           = "data"
   subscription_id = var.backend_cert_kv_sub_id
+  skip_credentials_validation = true
   features {}
 }
 provider "azurerm" {
