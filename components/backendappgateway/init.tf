c96876576b59f2d630bd078d293cd9d26a6deeff
Matt Slater <matt.slater@hmcts.net>
2021-02-22 19:28:27 +0000

    DTSPO-658 - remove subscription

--- components/backendappgateway/init.tf
+++ components/backendappgateway/init.tf
@@ -12,8 +12,8 @@ terraform {
   }
 }
 provider "azurerm" {
-  alias                      = "data"
- # subscription_id            = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
+  alias = "data"
+  # subscription_id            = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
   features {}
 }
 provider "azurerm" {
