259fb8240a6050d53b549c64284cc4422fc96960
Matt Slater <matt.slater@hmcts.net>
2021-02-23 08:58:20 +0000

    DTSPO-658 - tidy up

--- components/backendappgateway/init.tf
+++ components/backendappgateway/init.tf
@@ -14,8 +14,6 @@ terraform {
 provider "azurerm" {
   alias                       = "data"
   subscription_id             = var.backend_cert_kv_sub_id
-  skip_credentials_validation = true
-  skip_provider_registration  = true
   features {}
 }
 provider "azurerm" {
