f7100f11556732bbd4519bc30d6bf795433c6b67
Matt Slater <matt.slater@hmcts.net>
2021-02-28 20:59:24 +0000

    DTSPO-658 - fix backend ip

--- components/backendappgateway/main.tf
+++ components/backendappgateway/main.tf
@@ -4,7 +4,7 @@ module "logworkspace" {
 }
 
 module "backendappgateway" {
-  source = "git::https://github.com/hmcts/terraform-module-application-backend.git?ref=DTSPO-658-fix-features-issue"
+  source = "git::https://github.com/hmcts/terraform-module-application-backend.git?ref=DTSPO-658-enable-https"
 
   yaml_path = "${path.cwd}/../../environments/${var.env}/backend_lb_config.yaml"
 
