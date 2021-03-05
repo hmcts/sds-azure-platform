aac47697fe69202b99aef8880fd47c46c5d1947d
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2021-03-05 16:46:12 +0000

    Add formatting

--- components/backendappgateway/main.tf
+++ components/backendappgateway/main.tf
@@ -8,16 +8,16 @@ module "backendappgateway" {
 
   yaml_path = "${path.cwd}/../../environments/${var.env}/backend_lb_config.yaml"
 
-  env                        = var.env
+  env = var.env
   # subscription               = var.subscription
-  vault_name                 = var.vault_name
-  location                   = var.location
-  backend_pool_ip_addresses  = var.destinations
-  private_ip_address         = var.be_private_ip_address
+  vault_name                = var.vault_name
+  location                  = var.location
+  backend_pool_ip_addresses = var.destinations
+  private_ip_address        = var.be_private_ip_address
   # oms_env                    = var.oms_env
   log_analytics_workspace_id = module.logworkspace.workspace_id
-  vnet_rg                  = var.vnet_rg
-  vnet_name                = var.vnet_name
-  key_vault_resource_group = var.key_vault_resource_group
-  common_tags              = local.common_tags
+  vnet_rg                    = var.vnet_rg
+  vnet_name                  = var.vnet_name
+  key_vault_resource_group   = var.key_vault_resource_group
+  common_tags                = local.common_tags
 }
