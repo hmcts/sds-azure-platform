7fe73eca017278b9389922f8df4f3cf9309f6bd7
TomKimber <thomas.kimber@hmcts.net>
2021-06-23 16:15:51 +0100

    Bumped Azure provider to 2.64.0.

--- components/trafficmanager/init.tf
+++ components/trafficmanager/init.tf
@@ -7,13 +7,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.58.0"
+      version = "2.64.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-
 }
-
