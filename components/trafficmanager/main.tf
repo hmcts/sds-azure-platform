95aad40b67ca9f6ac0b13cbf64982127658f5f8c
TomKimber <thomas.kimber@hmcts.net>
2021-06-25 11:57:36 +0100

    Removed Traffic Manager comments.

--- components/trafficmanager/main.tf
+++ components/trafficmanager/main.tf
@@ -10,9 +10,7 @@ module "traffic_manager" {
 
   common_tags = module.ctags.common_tags
 
-  # Traffic Manager Endpoints
   traffic_manager_endpoints = var.traffic_manager_endpoints
 
-  # Traffic Manager Profiles
   traffic_manager_profiles = var.traffic_manager_profiles
 }
