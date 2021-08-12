1c8b2e33c44723187c16e5644006d5197fea187d
Zoe Cope <zoe.cope@hmcts.net>
2021-08-12 09:53:23 +0100

    fix sym link

---
+++ environments/variables.tf
@@ -0,0 +1,19 @@
+// terraform doesn't let you have undeclared vars used from a tfvars file
+// We link this file using a symbolic link  (`ln -s ../../environments/variables.tf variables.tf`) in each component directory
+
+variable "destinations" {}
+variable "env" {}
+variable "environment" {}
+variable "frontends" {}
+variable "location" {}
+variable "oms_env" {}
+variable "be_private_ip_address" {}
+variable "private_ip_address" {}
+variable "project" {}
+variable "subscription" {}
+variable "vnet_name" {}
+variable "vnet_rg" {}
+variable "product" {}
+variable "builtFrom" {}
+variable "traffic_manager_endpoints" { default = {} }
+variable "traffic_manager_profiles" { default = {} }
