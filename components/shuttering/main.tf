6fd2b3a398d37179609ab06f488fcc978041567a
Zoe Cope <zoe.cope@hmcts.net>
2021-08-12 10:48:33 +0100

    add vars

--- components/shuttering/main.tf
+++ components/shuttering/main.tf
@@ -3,7 +3,6 @@ module "shutter_page" {
   env                        = var.environment
   subscription               = var.subscription
   common_tags                = module.ctags.common_tags
-  shutter_page               = var.shutter_page
   shutter_rg                 = var.shutter_rg
   resource_group_name        = var.resource_group_name
   certificate_key_vault_name = var.certificate_key_vault_name
