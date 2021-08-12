209468debee61cf2c336b41d289fd3843beeac36
Zoe Cope <zoe.cope@hmcts.net>
2021-08-12 10:32:22 +0100

    add vars

--- components/shuttering/main.tf
+++ components/shuttering/main.tf
@@ -1,7 +1,10 @@
 module "shutter_page" {
-  source      = "git::https://github.com/hmcts/azure-platform-terraform.git//modules/shutter_page?ref=master"
-  env         = var.environment
-  common_tags = module.ctags.common_tags
+  source       = "git::https://github.com/hmcts/azure-platform-terraform.git//modules/shutter_page?ref=master"
+  env          = var.environment
+  common_tags  = module.ctags.common_tags
+  shutter_page = var.shutter_page
+  shutter_rg   = var.shutter_rg
+  cdn_sku      = var.cdn_sku
 
 }
 
