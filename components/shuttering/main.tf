47df850b4be902460721396ef3a5405cab19f47c
Zoe Cope <zoe.cope@hmcts.net>
2021-08-12 09:35:12 +0100

    DTSPO-4440 Add shutter module

---
+++ components/shuttering/main.tf
@@ -0,0 +1,13 @@
+module "shutter_page" {
+  source      = "git::https://github.com/hmcts/azure-platform-terraform/modules/shutter_page.git?ref=master"
+  env = var.environment
+  common_tags = module.ctags.common_tags
+
+}
+
+module "ctags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.environment
+  product     = var.product
+  builtFrom   = var.builtFrom
+}
