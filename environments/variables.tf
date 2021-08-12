72214413eaa9f43426edc488242bc530b01174c9
Zoe Cope <zoe.cope@hmcts.net>
2021-08-12 12:52:00 +0100

    add variables

--- environments/variables.tf
+++ environments/variables.tf
@@ -17,3 +17,8 @@ variable "product" {}
 variable "builtFrom" {}
 variable "traffic_manager_endpoints" { default = {} }
 variable "traffic_manager_profiles" { default = {} }
+variable "shutter_page" {}
+variable "shutter_rg" {}
+variable "resource_group_name" {}
+variable "certificate_key_vault_name" {}
+variable "cdn_sku" {}
