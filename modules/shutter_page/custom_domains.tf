3534e10c8c1d153368cd2b5387077e13ad78ff7e
msl8r <72858053+msl8r@users.noreply.github.com>
2021-08-17 09:47:58 +0100

    DTSPO-4365 - migrate to terraform resource rather than arm deployment

--- modules/shutter_page/custom_domains.tf
+++ modules/shutter_page/custom_domains.tf
@@ -1,20 +1,8 @@
-data "template_file" "customdomain" {
-  template = file("${path.module}/templates/custom_domain.json")
-}
-
-resource "azurerm_template_deployment" "custom_domain" {
-  for_each = { for frontend in var.shutter_apps : frontend.name => frontend if !contains(["jui-redirect", "fact-redirect"], frontend.name)
+resource "azurerm_cdn_endpoint_custom_domain" "this" {
+  for_each = { for frontend in var.shutter_apps : frontend.name => frontend
   }
 
-  template_body       = data.template_file.customdomain.rendered
-  name                = split(".", replace("${each.value.custom_domain}", "www.", ""))[0]
-  resource_group_name = data.azurerm_resource_group.shutter.name
-  deployment_mode     = "Incremental"
-
-  parameters = {
-    name     = "${azurerm_cdn_profile.main["${each.value.product}"].name}/${azurerm_cdn_endpoint.shutter_endpoint["${each.value.name}"].name}/${replace("${each.value.custom_domain}", ".", "-")}"
-    hostName = "${each.value.custom_domain}"
-  }
-
-  depends_on = [azurerm_cdn_endpoint.shutter_endpoint]
+  name            = split(".", replace("${each.value.custom_domain}", "www.", ""))[0]
+  cdn_endpoint_id = azurerm_cdn_endpoint.shutter_endpoint[each.value.name].id
+  host_name       = each.value.custom_domain
 }
