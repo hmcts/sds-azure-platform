b3d8ff2922b7142a62104bfad92f72d181a17cd5
Zoe Cope <zoe.cope@hmcts.net>
2021-10-08 09:55:23 +0100

    amend variable

--- environments/variable.tf
+++ environments/variable.tf
@@ -18,4 +18,4 @@ variable "traffic_manager_endpoints" { default = {} }
 variable "traffic_manager_profiles" { default = {} }
 variable "shutter_rg" { default = "" }
 variable "cdn_sku" { default = "" }
-variable "availability_zones" { default = "" }
+variable "availability_zones" { default = [] }
