e140199d0c59f25fe5c68eb52de4c230760e62bb
Zoe Cope <zoe.cope@hmcts.net>
2021-10-11 10:31:07 +0100

    DTSPO-4687 Add availability zones

--- components/appgateway/main.tf
+++ components/appgateway/main.tf
@@ -15,16 +15,17 @@ module "appgateway" {
   source = "git::https://github.com/hmcts/terraform-module-applicationgateway.git?ref=master"
 
 
-  env                        = var.env
-  subscription               = var.subscription
-  location                   = var.location
-  private_ip_address         = var.private_ip_address
-  destinations               = var.destinations
-  frontends                  = var.frontends
-  common_tags                = module.ctags.common_tags
-  oms_env                    = var.oms_env
-  project                    = var.project
-  vnet_rg                    = var.vnet_rg
-  vnet_name                  = var.vnet_name
-  log_analytics_workspace_id = module.logworkspace.workspace_id
+  env                                = var.env
+  subscription                       = var.subscription
+  location                           = var.location
+  private_ip_address                 = var.private_ip_address
+  destinations                       = var.destinations
+  frontends                          = var.frontends
+  common_tags                        = module.ctags.common_tags
+  oms_env                            = var.oms_env
+  project                            = var.project
+  vnet_rg                            = var.vnet_rg
+  vnet_name                          = var.vnet_name
+  log_analytics_workspace_id         = module.logworkspace.workspace_id
+  enable_multiple_availability_zones = true
 }
