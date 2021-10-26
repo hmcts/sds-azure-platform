821ea6e55406766d782471e98484214aaee29f39
Thomas Thornton <>
2021-10-26 11:39:40 +0100

    terraform fmt

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -1,5 +1,3 @@
-
-
 module "ctags" {
   source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
   environment = var.environment
@@ -8,15 +6,15 @@ module "ctags" {
 }
 
 module "api-mgmt-private" {
-  source                     = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
-  location                   = var.location
-  sku_name                   = "Developer"
-  vnet_rg                    = var.vnet_rg
-  vnet_name                  = var.vnet_name
-  env                        = var.env
-  virtualNetworkType         = "Internal"
-  department                 = var.department
-  common_tags                = module.ctags.common_tags
+  source             = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
+  location           = var.location
+  sku_name           = "Developer"
+  vnet_rg            = var.vnet_rg
+  vnet_name          = var.vnet_name
+  env                = var.env
+  virtualNetworkType = "Internal"
+  department         = var.department
+  common_tags        = module.ctags.common_tags
 }
 
 resource "azurerm_api_management_named_value" "environment-named-value-private" {
