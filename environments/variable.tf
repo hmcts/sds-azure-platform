8670b4f09f45957a83625c357f6175c9a4b15556
Darren Bromwich <d.bromwich@kainos.com>
2021-10-26 10:05:19 +0100

    update to symbolic link example command

--- environments/variable.tf
+++ environments/variable.tf
@@ -1,5 +1,5 @@
 // terraform doesn't let you have undeclared vars used from a tfvars file
-// We link this file using a symbolic link  (`ln -s ../../environments/variables.tf variables.tf`) in each component directory
+// We link this file using a symbolic link  (`ln -s ../../environments/variable.tf variable.tf`) in each component directory
 
 variable "destinations" {}
 variable "env" {}
