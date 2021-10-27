b63dc564d37ccb808a0d2c4b82c06c263dbab01f
Thomas Thornton <>
2021-10-27 13:56:53 +0100

    fmt

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -6,15 +6,15 @@ module "ctags" {
 }
 
 module "api-mgmt" {
-  source               = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
-  location             = var.location
-  sku_name             = var.apim_sku_name
-  virtual_network_rg   = var.vnet_rg
-  virtual_network_name = var.vnet_name
-  environment          = var.env
-  virtual_network_type = "Internal"
-  department           = var.department
-  common_tags          = module.ctags.common_tags
+  source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
+  location                       = var.location
+  sku_name                       = var.apim_sku_name
+  virtual_network_resource_group = var.vnet_rg
+  virtual_network_name           = var.vnet_name
+  environment                    = var.env
+  virtual_network_type           = "Internal"
+  department                     = var.department
+  common_tags                    = module.ctags.common_tags
 }
 
 resource "azurerm_api_management_named_value" "environment" {
