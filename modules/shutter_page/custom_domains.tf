eae8e6f06c3f1c8a86b095576ec7cf0ab7b3aa28
msl8r <72858053+msl8r@users.noreply.github.com>
2022-01-26 15:45:56 +0000

    Deploy enable https custom endpoint with native tf resource

--- modules/shutter_page/custom_domains.tf
+++ modules/shutter_page/custom_domains.tf
@@ -1,3 +1,11 @@
+data "azurerm_key_vault_secret" "certificate" {
+  for_each = { for frontend in var.shutter_apps : frontend.name => frontend
+  }
+
+  name         = each.value.certificate_name
+  key_vault_id = data.azurerm_key_vault.certificate_vault.id
+}
+
 resource "azurerm_cdn_endpoint_custom_domain" "this" {
   for_each = { for frontend in var.shutter_apps : frontend.name => frontend
   }
@@ -5,4 +13,9 @@ resource "azurerm_cdn_endpoint_custom_domain" "this" {
   name            = replace(each.value.custom_domain, ".", "-")
   cdn_endpoint_id = azurerm_cdn_endpoint.shutter_endpoint[each.value.name].id
   host_name       = each.value.custom_domain
+
+  user_managed_https {
+    key_vault_certificate_id = data.azurerm_key_vault_secret.certificate[each.value.name].id
+  }
 }
+
