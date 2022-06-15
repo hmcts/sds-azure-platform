7e7616796754c8efaf7bdc529c71ca4fb0a12378
Enda Kelly <enda.kelly@hmcts.net>
2022-06-15 15:47:14 +0100

    Update azurerm provider for backend appgw

--- components/backendappgateway/main.tf
+++ components/backendappgateway/main.tf
@@ -17,7 +17,7 @@ locals {
 }
 
 module "backendappgateway" {
-  source = "git::https://github.com/hmcts/terraform-module-application-backend.git?ref=master"
+  source = "git::https://github.com/hmcts/terraform-module-application-backend.git?ref=terraform-azurerm-v3"
 
   yaml_path = "${path.cwd}/../../environments/${var.env}/backend_lb_config.yaml"
 
