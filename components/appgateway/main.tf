397b34330e5e5b214c4083deeb379f008ae57741
Enda Kelly <enda.kelly@hmcts.net>
2022-06-22 16:28:30 +0100

    add av zone to public ip

--- components/appgateway/main.tf
+++ components/appgateway/main.tf
@@ -15,17 +15,17 @@ module "appgateway" {
   source = "git::https://github.com/hmcts/terraform-module-applicationgateway.git?ref=terraform-azurerm-v3-sds"
 
 
-  env                                = var.env
-  subscription                       = var.subscription
-  location                           = var.location
-  private_ip_address                 = var.private_ip_address
-  destinations                       = var.destinations
-  frontends                          = var.frontends
-  common_tags                        = module.ctags.common_tags
-  oms_env                            = var.oms_env
-  project                            = var.project
-  vnet_rg                            = var.vnet_rg
-  vnet_name                          = var.vnet_name
-  log_analytics_workspace_id         = module.logworkspace.workspace_id
-  enable_multiple_availability_zones = true
+  env                                          = var.env
+  subscription                                 = var.subscription
+  location                                     = var.location
+  private_ip_address                           = var.private_ip_address
+  destinations                                 = var.destinations
+  frontends                                    = var.frontends
+  common_tags                                  = module.ctags.common_tags
+  oms_env                                      = var.oms_env
+  project                                      = var.project
+  vnet_rg                                      = var.vnet_rg
+  vnet_name                                    = var.vnet_name
+  log_analytics_workspace_id                   = module.logworkspace.workspace_id
+  public_ip_enable_multiple_availability_zones = var.public_ip_enable_multiple_availability_zones
 }
