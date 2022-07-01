398189a8b69547f29956476699d53abe99da4167
Christopher Pateman <christopher.pateman@version1.com>
2022-07-01 10:04:42 +0100

    add cdn ssl for apps in sbox

--- modules/shutter_page/custom_domains.tf
+++ modules/shutter_page/custom_domains.tf
@@ -10,7 +10,6 @@ resource "azurerm_cdn_endpoint_custom_domain" "this" {
     for_each = lookup(each.value, "cdn_ssl_mode", var.ssl_mode) == "AzureKeyVault" ? { key_vault_certificate_id = "" } : {}
     content {
       key_vault_certificate_id = data.azurerm_key_vault_secret.certificate[each.value["name"]].id
-      tls_version              = "TLS12"
     }
   }
 
