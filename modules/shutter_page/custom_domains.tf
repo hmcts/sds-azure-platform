745dbdf084885b78bba6080b69665efd3689af83
msl8r <72858053+msl8r@users.noreply.github.com>
2022-07-04 14:56:41 +0100

    DTSPO-9050 - fix formatting

--- modules/shutter_page/custom_domains.tf
+++ modules/shutter_page/custom_domains.tf
@@ -7,12 +7,12 @@ resource "azurerm_cdn_endpoint_custom_domain" "this" {
   host_name       = each.value.custom_domain
     
   lifecycle {
-  ignore_changes = [
-    # Ignore changes user_managed_https until we have another solution.
-    # Reason: Null resource is used to config the cert config and since the PR below has been merged every time the
-    # the pipeline is ran it removes the config because terraform wants to manage it.
-    # https://github.com/hashicorp/terraform-provider-azurerm/pull/13283
-    user_managed_https,
-  ]
-}
+    ignore_changes = [
+      # Ignore changes user_managed_https until we have another solution.
+      # Reason: Null resource is used to config the cert config and since the PR below has been merged every time the
+      # the pipeline is ran it removes the config because terraform wants to manage it.
+      # https://github.com/hashicorp/terraform-provider-azurerm/pull/13283
+      user_managed_https,
+    ]
+  }
 }
