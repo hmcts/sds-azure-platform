7e3bfc95e1eef7262e8e3220e4c99b75e72b9e11
Matt Slater <72858053+msl8r@users.noreply.github.com>
2022-12-30 11:32:33 +0000

    DTSPO-11790 - add expiresAfter tag

--- components/appgateway/main.tf
+++ components/appgateway/main.tf
@@ -5,10 +5,11 @@ module "logworkspace" {
 }
 
 module "ctags" {
-  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
-  environment = var.environment
-  product     = var.product
-  builtFrom   = var.builtFrom
+  source       = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment  = var.environment
+  product      = var.product
+  builtFrom    = var.builtFrom
+  expiresAfter = var.expiresAfter
 }
 
 module "appgateway" {
