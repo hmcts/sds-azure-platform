09b32c3fc078a685d95e2730c4f305445b0d9445
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-05-17 15:28:41 +0100

    corrected keyvault name

--- components/apim_appgw/main.tf
+++ components/apim_appgw/main.tf
@@ -15,7 +15,7 @@ module "logworkspace" {
 data "azurerm_subscription" "current" {}
 
 locals {
-  key_vault_name = "acmedcdcftapps${var.subscription}"
+  key_vault_name = "acmedtssds${var.subscription}"
   dns_zone       = (var.env == "sbox") ? "sandbox" : var.env
 }
 
