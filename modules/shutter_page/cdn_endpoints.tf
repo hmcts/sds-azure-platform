7d4266d2553f3a6098a975b394e2cda1b170859d
Rees Pozzi <reespozzi@icloud.com>
2023-05-31 15:34:17 +0100

    Re-add missed -

--- modules/shutter_page/cdn_endpoints.tf
+++ modules/shutter_page/cdn_endpoints.tf
@@ -2,7 +2,7 @@ resource "azurerm_cdn_endpoint" "shutter_endpoint" {
   for_each = { for frontend in var.shutter_apps : frontend.name => frontend
   }
   # Option to override shutter name to stop CFT clashes
-  name                   = each.value.shutter_name_override != "false" ? substr("hmcts-${each.value.shutter_name_override}-shutter-${var.env}", 0, 48) : substr("hmcts${split(".", replace("${each.value.custom_domain}", "www.", ""))[0]}-shutter-${var.env}", 0, 48)
+  name                   = each.value.shutter_name_override != "false" ? substr("hmcts-${each.value.shutter_name_override}-shutter-${var.env}", 0, 48) : substr("hmcts-${split(".", replace("${each.value.custom_domain}", "www.", ""))[0]}-shutter-${var.env}", 0, 48)
   profile_name           = azurerm_cdn_profile.main["${each.value.name}"].name
   location               = "West US"
   resource_group_name    = data.azurerm_resource_group.shutter.name
