468823597b985f5321058ea8f05a327388be7585
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2023-07-06 15:01:22 +0100

    testing new frontdoor

--- environments/variable.tf
+++ environments/variable.tf
@@ -6,6 +6,7 @@ variable "env" {}
 variable "environment" {}
 variable "frontends" {}
 variable "new_frontends" {}
+variable "health_probe" {}
 variable "location" {}
 variable "oms_env" {}
 variable "private_ip_address" {}
