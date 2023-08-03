49fd22d336e5a6bf5fadf5b7cb37fa6f773f6ecf
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-08-03 11:47:02 +0100

    testing

--- components/apim_appgw/download_root_certs.bash
+++ components/apim_appgw/download_root_certs.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # cd .terraform/modules/app-gw
 #grabing the public root CA 
-
+ls
 curl https://letsencrypt.org/certs/isrgrootx1.pem >root.pem
 #grabing the public intermediate CA
 curl https://letsencrypt.org/certs/lets-encrypt-r3.pem >intermediate.pem
 
 cat root.pem intermediate.pem >merged.pem
-
+ls
 echo -n "{\"message\":\"No output\"}"
