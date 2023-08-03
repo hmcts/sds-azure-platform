74e6fd7c5d661dcd9eac270d13c332cb6956b412
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-08-03 13:36:45 +0100

    empty merged file

--- components/apim_appgw/download_root_certs.bash
+++ components/apim_appgw/download_root_certs.bash
@@ -9,6 +9,5 @@ curl https://letsencrypt.org/certs/isrgrootx1.pem >root.pem
 curl https://letsencrypt.org/certs/lets-encrypt-r3.pem >intermediate.pem
 
 cat root.pem intermediate.pem >merged.pem
-ls
-rm root.pem intermediate.pem
+
 echo -n "{\"data\":\"nothing\"}"
