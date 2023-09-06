bfbde9a3eb0a861a361abcd3647acbf51b895974
Thomas Thornton <>
2023-09-06 13:41:00 +0100

    allow apim appgw to have multiple ssl profiles

--- components/apim_appgw/download_root_certs.bash
+++ components/apim_appgw/download_root_certs.bash
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
 
-#grabing the public root CA 
+#grabing the Lets Encrypt public root CA 
 
 curl https://letsencrypt.org/certs/isrgrootx1.pem >root.pem
 #grabing the public intermediate CA
 curl https://letsencrypt.org/certs/lets-encrypt-r3.pem >intermediate.pem
 
 cat root.pem intermediate.pem >merged.pem
+
+
+######
+curl https://letsencrypt.org/certs/isrgrootx1.pem >roo2.pem
+#grabing the public intermediate CA
+curl https://letsencrypt.org/certs/lets-encrypt-r3.pem >intermediate2.pem
+
+cat root2.pem intermediate2.pem >merged2.pem
 
 echo -n "{\"data\":\"nothing\"}"
--- components/apim_appgw/main.tf
+++ components/apim_appgw/main.tf
@@ -27,7 +27,7 @@ module "app-gw" {
     azurerm.kv  = azurerm.kv
   }
 
-  source                                       = "git::https://github.com/hmcts/terraform-module-apim-application-gateway.git?ref=main"
+  source                                       = "git::https://github.com/hmcts/terraform-module-apim-application-gateway.git?ref=multiple-ssl-profile "
   yaml_path                                    = "${path.cwd}/../../environments/${local.env}/apim_appgw_config.yaml"
   env                                          = local.dns_zone
   location                                     = var.location
@@ -47,7 +47,7 @@ module "app-gw" {
   project_name                                 = var.project
   min_capacity                                 = var.apim_appgw_min_capacity
   max_capacity                                 = var.apim_appgw_max_capacity
-  trusted_client_certificate_data              = file("${path.module}/merged.pem")
+  trusted_client_certificate_data              = var.trusted_client_certificate_data
   depends_on                                   = [data.external.bash_script]
 }
 
