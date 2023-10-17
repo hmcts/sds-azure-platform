536445e7c65a1e7041d4b012cb4ca4f66053264f
paulridout <84783658+paulridout@users.noreply.github.com>
2023-10-17 09:59:48 +0100

    Updated application gateway config (#584)
    * Updated application gateway config
    
    Updated app-gw module in components/apim_appgw/main.tf in line with changes made to terraform-module-apim-application-gateway.
    
    Removed application gateway configuration files as no projects are currently using application gateway in SDS.
    
    * Updated application gateway config
    
    Fixed formatting issue in app gateway main.tf.
    
    * Updated application gateway config
    
    Added back application gateway config files.  Files are currently empty.
    
    * Updated application gateway config
    
    Added gateways property to app gateway configuration files.
    
    * Updated application gateway config
    
    Removed hyphen from front of gateways property in application gateway config files.
    
    * Updated application gateway config
    
    Added ssl_certificates section to app gateway configuration files
    
    * Updated application gateway config
    
    Removed gateway_configuration section and corrected indentation of ssl_certificates section in app gateway configuration files
    
    * Updated application gateway config
    
    Added back all configuration to app gateway configuration files except mTLS configuration.
    
    * Updated application gateway config
    
    Added ssl_certificate_name property to app gateway configuration files.
    
    ---------
    
    Co-authored-by: Tom Kimber <thomas.kimber@hmcts.net>

--- components/apim_appgw/main.tf
+++ components/apim_appgw/main.tf
@@ -47,8 +47,13 @@ module "app-gw" {
   project_name                                 = var.project
   min_capacity                                 = var.apim_appgw_min_capacity
   max_capacity                                 = var.apim_appgw_max_capacity
-  trusted_client_certificate_data              = file("${path.module}/merged.pem")
-  depends_on                                   = [data.external.bash_script]
+  trusted_client_certificate_data = {
+    "lets_encrypt" = {
+      path = file("${path.module}/merged.pem")
+    }
+  }
+
+  depends_on = [data.external.bash_script]
 }
 
 data "external" "bash_script" {
