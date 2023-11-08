e447e43b955c953e90d4cbfcf94b53e6bbb40e6c
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-11-08 23:43:16 +0000

    updated init

--- components/frontdoor/init.tf
+++ components/frontdoor/init.tf
@@ -2,11 +2,7 @@ terraform {
   required_version = ">= 1.6.0"
 
   backend "azurerm" {
-    resource_group_name  = "azure-control-prod-rg"
-    storage_account_name = "c5ca62022e7536c8d566csa"
-    container_name       = "subscription-tfstate"
-    key                  = "UK South/sds-platform/sds-azure-platform/prod/frontdoor/terraform.tfstate"
-    subscription_id      = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
+    subscription_id = "04d27a32-7a07-48b3-95b8-3c8691e1a263"
   }
   required_providers {
     azurerm = {
