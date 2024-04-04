f4c0d02de95ecc34b782b6ccabbafe13dd12b1dd
Alex Bance <Alex.Bance@justice.gov.uk>
2024-04-04 13:37:17 +0100

    feat: change module branch ref

--- components/backendappgateway/main.tf
+++ components/backendappgateway/main.tf
@@ -20,7 +20,7 @@ locals {
 }
 
 module "backendappgateway" {
-  source = "git::https://github.com/hmcts/terraform-module-application-backend.git?ref=master"
+  source = "git::https://github.com/hmcts/terraform-module-application-backend.git?ref=feat%2Fspecify-timeout"
 
   yaml_path = "${path.cwd}/../../environments/${var.env}/backend_lb_config.yaml"
 
