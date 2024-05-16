2fe1744f66824d58dfbb692df84467948be3e391
Joshua Glass <141819030+JoshuaGlassKainos@users.noreply.github.com>
2024-05-16 10:48:53 +0100

    DTSPO-17136-apim-upgrade-improvements (#775)
    * changed vars to object

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -17,8 +17,7 @@ module "api-mgmt" {
   department                     = var.department
   common_tags                    = module.ctags.common_tags
   route_next_hop_in_ip_address   = local.hub[var.hub].ukSouth.next_hop_ip
-  trigger_migration              = var.trigger_migration
-  trigger_migration_temp_pip     = var.trigger_migration_temp_pip
+  migration_variables            = var.migration_variables
 }
 
 resource "azurerm_api_management_named_value" "environment" {
