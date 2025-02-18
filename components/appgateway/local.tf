1be6ff30c7df8451bd92c9cb05737d187d8f3895
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2025-02-18 14:21:20 +0000

    fixing rg name

--- components/appgateway/local.tf
+++ components/appgateway/local.tf
@@ -1,7 +1,7 @@
 locals {
   hub_env                  = (var.env == "demo" || var.env == "dev" || var.env == "ithc" || var.env == "test") ? "nonprod" : (var.env == "aat" || var.env == "stg") ? "prod" : var.env
   key_vault_name           = "acmedtssds${var.subscription}"
-  key_vault_resource_group = "cft-platform-${var.subscription}-rg"
+  key_vault_resource_group = "sds-platform-${var.subscription}-rg"
 
   hub = {
     sbox = {
