3396585841321943f40c4d8aa7315f5e44279465
Dawid Strozak <dawid.strozak@hmcts.net>
2025-03-05 17:13:30 +0000

    Revert attempt to pass alert_location

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -8,7 +8,6 @@ module "ctags" {
 
 module "api-mgmt" {
   source                         = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
-  alert_location                 = "global"
   location                       = var.location
   sku_name                       = var.apim_sku_name
   virtual_network_resource_group = var.vnet_rg
