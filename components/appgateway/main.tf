591fc8f41aaf70e6dabc216614a5ab3aa7611c5d
Alex Bance <Alex.Bance@justice.gov.uk>
2025-08-06 13:43:39 +0100

    fix: multi az for be app gw

--- components/appgateway/main.tf
+++ components/appgateway/main.tf
@@ -23,23 +23,24 @@ module "appgateway" {
   source = "git::https://github.com/hmcts/terraform-module-applicationgateway.git?ref=master"
 
 
-  env                                = var.env
-  subscription                       = var.subscription
-  location                           = var.location
-  private_ip_address                 = var.private_ip_address
-  destinations                       = var.destinations
-  frontends                          = var.frontends
-  common_tags                        = module.ctags.common_tags
-  oms_env                            = var.oms_env
-  project                            = var.project
-  vnet_rg                            = var.vnet_rg
-  vnet_name                          = var.vnet_name
-  log_analytics_workspace_id         = module.logworkspace.workspace_id
-  diagnostics_storage_account_id     = data.azurerm_storage_account.diagnostics.id
-  enable_multiple_availability_zones = var.env == "sbox" ? false : true
-  vault_name                         = local.key_vault_name
-  key_vault_resource_group           = local.key_vault_resource_group
-  ssl_certificate_name               = var.ssl_certificate
+  env                                          = var.env
+  subscription                                 = var.subscription
+  location                                     = var.location
+  private_ip_address                           = var.private_ip_address
+  destinations                                 = var.destinations
+  frontends                                    = var.frontends
+  common_tags                                  = module.ctags.common_tags
+  oms_env                                      = var.oms_env
+  project                                      = var.project
+  vnet_rg                                      = var.vnet_rg
+  vnet_name                                    = var.vnet_name
+  log_analytics_workspace_id                   = module.logworkspace.workspace_id
+  diagnostics_storage_account_id               = data.azurerm_storage_account.diagnostics.id
+  public_ip_enable_multiple_availability_zones = true
+  enable_multiple_availability_zones           = true
+  vault_name                                   = local.key_vault_name
+  key_vault_resource_group                     = local.key_vault_resource_group
+  ssl_certificate_name                         = var.ssl_certificate
 
   # Control the rollout of the TLS 1.0/1.1 deprecation, the ternary should be removed once the rollout is complete
   ssl_policy = var.env == "sbox" ? var.ssl_policy : local.current_ssl_policy
