acaf7b6990de972318055c5e16eb530c4ebdd931
ieuanb74 <144245358+ieuanb74@users.noreply.github.com>
2025-09-11 10:20:00 +0100

    Send logs to log analytics in Demo backend appgw (#969)

--- components/backendappgateway/main.tf
+++ components/backendappgateway/main.tf
@@ -24,17 +24,18 @@ module "backendappgateway" {
 
   yaml_path = "${path.cwd}/../../environments/${var.env}/backend_lb_config.yaml"
 
-  env                        = var.env
-  vault_name                 = local.vault_name
-  location                   = var.location
-  backend_pool_ip_addresses  = var.destinations
-  private_ip_address         = local.gateways[*].gateway_configuration.private_ip_address
-  log_analytics_workspace_id = module.logworkspace.workspace_id
-  vnet_rg                    = var.vnet_rg
-  vnet_name                  = var.vnet_name
-  key_vault_resource_group   = "sds-platform-${var.environment}-rg"
-  ssl_policy                 = var.ssl_policy
-  common_tags                = module.ctags.common_tags
+  env                               = var.env
+  vault_name                        = local.vault_name
+  location                          = var.location
+  backend_pool_ip_addresses         = var.destinations
+  private_ip_address                = local.gateways[*].gateway_configuration.private_ip_address
+  log_analytics_workspace_id        = module.logworkspace.workspace_id
+  send_access_logs_to_log_analytics = var.send_access_logs_to_log_analytics
+  vnet_rg                           = var.vnet_rg
+  vnet_name                         = var.vnet_name
+  key_vault_resource_group          = "sds-platform-${var.environment}-rg"
+  ssl_policy                        = var.ssl_policy
+  common_tags                       = module.ctags.common_tags
 
   diagnostics_storage_account_id     = data.azurerm_storage_account.diagnostics.id
   enable_multiple_availability_zones = true
