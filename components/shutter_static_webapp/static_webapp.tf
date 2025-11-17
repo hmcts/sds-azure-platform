80ecef2d34fdef2b5e61111206bc8e7c06e98cfe
Asaleem-hmcts <Aamir.Saleem@HMCTS.NET>
2025-11-17 20:33:16 +0000

    test

--- components/shutter_static_webapp/static_webapp.tf
+++ components/shutter_static_webapp/static_webapp.tf
@@ -10,8 +10,7 @@ module "static_webapp" {
   providers = {
     azurerm         = azurerm
     azurerm.dnszone = azurerm.dnszone
-    #github          = github
-    #.module
+    github          = github.module
   }
   source              = "git::https://github.com/hmcts/terraform-module-shutter-static-webapp.git?ref=feature/testing"
   shutter_apps        = local.shutter_apps
