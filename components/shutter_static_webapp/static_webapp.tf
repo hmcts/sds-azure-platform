d74f130f5a68db8072d134bf9ee03ad87c8630a0
Asaleem-hmcts <Aamir.Saleem@HMCTS.NET>
2025-11-17 15:58:35 +0000

    test-pus

--- components/shutter_static_webapp/static_webapp.tf
+++ components/shutter_static_webapp/static_webapp.tf
@@ -21,6 +21,7 @@ module "static_webapp" {
   sku_size            = var.sku_size
 }
 
+
 module "ctags" {
   source       = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
   environment  = var.env
