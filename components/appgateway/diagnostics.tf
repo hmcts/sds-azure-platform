17099ed575ad300f4ec1a1a1dd35e47caa8ecff5
ashleighB31 <Ashleigh.Brennan@justice.gov.uk>
2026-06-12 14:37:19 +0100

    enable loggin for application gateway errors

--- components/appgateway/diagnostics.tf
+++ components/appgateway/diagnostics.tf
@@ -2,3 +2,42 @@ data "azurerm_storage_account" "diagnostics" {
   name                = "hmctssdsdiag${var.env}"
   resource_group_name = "lz-${var.env}-rg"
 }
+
+resource "azurerm_monitor_diagnostic_setting" "appgateway" {
+  name                       = "appgw-diagnostics-${var.env}"
+  target_resource_id         = module.appgateway.application_gateway_id
+  storage_account_id         = data.azurerm_storage_account.diagnostics.id
+  log_analytics_workspace_id = module.logworkspace.workspace_id
+
+  enabled_log {
+    category = "ApplicationGatewayAccessLog"
+    retention_policy {
+      enabled = true
+      days    = 90
+    }
+  }
+
+  enabled_log {
+    category = "ApplicationGatewayPerformanceLog"
+    retention_policy {
+      enabled = true
+      days    = 90
+    }
+  }
+
+  enabled_log {
+    category = "ApplicationGatewayFirewallLog"
+    retention_policy {
+      enabled = true
+      days    = 90
+    }
+  }
+
+  metric {
+    category = "AllMetrics"
+    retention_policy {
+      enabled = true
+      days    = 90
+    }
+  }
+}
