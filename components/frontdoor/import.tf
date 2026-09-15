5b91320fb42e6a78ae2d01ac99db1827641bb560
JamieMcConnellKainos <jamie.mcconnell@kainos.com>
2026-09-15 14:03:42 +0100

    Added import blocks for resources (#1153)
    * Added import blocks for resources
    
    * Imports made 'prod' env specific
    
    ---------
    
    Co-authored-by: Jamie McConnell <jamie.mcconnell@justice.gov.uk>

---
+++ components/frontdoor/import.tf
@@ -0,0 +1,15 @@
+import {
+  for_each = var.env == "prod" ? ["opal-frontend"] : []
+
+  to = module.premium_front_door.azurerm_dns_txt_record.public_dns_record["opal-frontend"]
+  id = "/subscriptions/ed302caf-ec27-4c64-a05e-85731c3ce90e/resourceGroups/reformmgmtrg/providers/Microsoft.Network/dnsZones/apps.hmcts.net/TXT/_dnsauth.opal-frontend"
+}
+
+import {
+  for_each = var.env == "prod" ? ["opal-rm-frontend"] : []
+
+  to = module.premium_front_door.azurerm_dns_txt_record.public_dns_record["opal-rm-frontend"]
+  id = "/subscriptions/ed302caf-ec27-4c64-a05e-85731c3ce90e/resourceGroups/reformmgmtrg/providers/Microsoft.Network/dnsZones/apps.hmcts.net/TXT/_dnsauth.opal-rm-frontend"
+}
+
+
